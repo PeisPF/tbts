e40918ac5a72dcc368928ed4d3582657a39586ac
Ste <schiesa.ies@gmail.com>
2018-06-24 00:54:32 +0200

    fix per errore di commit

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+    private Animator mAnimator;
+    private PlayerMove mv;
+    // Use this for initialization
+    void Start () {
+        mAnimator = GetComponent<Animator>();
+        GameObject thePlayer = GameObject.Find("Player");
+        mv = thePlayer.GetComponent<PlayerMove>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bool walking = mv.isMoving();
+        mAnimator.SetBool("walking", walking);
+        if (mv.isOpeningADoor())
+        {
+            mv.setOpeningADoor(false);
+            mAnimator.SetTrigger("openingADoor");
+        }
+    }
+}
