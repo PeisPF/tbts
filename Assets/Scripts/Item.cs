1fdbb787ff0c87eee747b0a07bf3276a51d457c5
Matteo Pavesi <matteo.pavesi@gamesys.co.uk>
2018-06-24 23:47:21 +0100

    [TBTS-38] onmouseover, primo tentativo non funzionante
    [TBTS-38] onmouseover, primo tentativo non funzionante

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -8,11 +8,13 @@ public abstract class Item : MonoBehaviour {
 
     public string[] actions;
     public int[] actionCosts;
+    public bool isHovered;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         actions = InitActions();
         actionCosts = InitActionCosts();
+        isHovered = false;
 	}
 
     public abstract int[] InitActionCosts();
@@ -20,7 +22,13 @@ public abstract class Item : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+        
+        if(this.isHovered)
+        {
+            
+            float emission = 0.5f;
+            GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.yellow * emission);
+        }
 	}
 
     public void Interact(int index)
@@ -66,5 +74,10 @@ public abstract class Item : MonoBehaviour {
     public virtual void Interact5()
     {
         throw new NotImplementedException();
+    }
+
+    public void OnMouseOver(){
+        Debug.Log("onmouseover" + this.name);
+        this.isHovered = true;
     }
 }
