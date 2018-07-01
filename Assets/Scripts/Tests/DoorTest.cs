4acae2dbc86ae2b44a608c380d826404cf402aa5
mfauccig <mfauccig@cern.ch>
2018-07-01 03:26:19 +0100

    Secondo test

--- Assets/Scripts/Tests/DoorTest.cs
+++ Assets/Scripts/Tests/DoorTest.cs
@@ -20,4 +20,22 @@ public class DoorTest{
 
         Assert.IsFalse(movingDoor);
     }
+
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator DoorWithUnitNextToItDoesNotMove()
+    {
+        PlayerMove unit = new PlayerMove();
+        var door = new GameObject().AddComponent<DoorScript>();
+        unit.InteractWithItem(door, 1);
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+
+        bool movingDoor = door.moving;
+
+        Assert.IsFalse(movingDoor);
+    }
 }
