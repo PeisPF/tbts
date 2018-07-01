3b96b9eb546f968b6c7cb0a1a09d0949480cb9d3
Iacopo <iacopo.arduini@gmail.com>
2018-07-01 23:24:24 +0200

    Creato menu principale con (attualmente funzionanti) i tasti new game e quit

---
+++ Assets/Scripts/Menu/ButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.GetComponent<Text>().color = Color.white;
+    }
+
+    private void Start()
+    {
+        this.GetComponent<Text>().color = Color.grey;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.GetComponent<Text>().color = Color.grey;
+    }
+}
