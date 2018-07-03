bd926462ff07e2a70dbbf2681965d872bb270354
Iacopo <iacopo.arduini@gmail.com>
2018-07-03 21:41:53 +0200

    aggiunta intro e credits

---
+++ Assets/Scripts/Menu/LoadNextSceneOnClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextSceneOnClick : MonoBehaviour {
+
+    public int indexOfNextScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            if (indexOfNextScene == -1)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(indexOfNextScene);
+            }
+        }
+            
+    }
+}
